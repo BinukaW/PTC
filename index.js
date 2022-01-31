3bb69a829e3df810d1c953f6307910dd3242c9fc
8nd8 <88880649+8nd8@users.noreply.github.com>
2022-01-31 18:26:55 +1100

    Post.js and User.js model files added

--- index.js
+++ index.js
@@ -1,46 +1,25 @@
 const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
-const mongoose = require('mongoose');
 
 const Post = require('./models/Post');
-const { MONGODB } = require('./config.js');
 
 const typeDefs = gql`
-  type Post {
-    id: ID!
-    body: String!
-    createdAt: String!
-    username: String!
-  }
-  type Query {
-    getPosts: [Post]
-  }
+    type Query {
+        sayHi: String!
+    }
 `;
 
 const resolvers = {
-  Query: {
-    async getPosts() {
-      try {
-        const posts = await Post.find();
-        return posts;
-      } catch (err) {
-        throw new Error(err);
-      }
+    Query: {
+        sayHi: () => 'Hello World!'
     }
-  }
 };
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers
+    typeDefs,
+    resolvers
 });
 
-mongoose
-  .connect(MONGODB, { useNewUrlParser: true })
-  .then(() => {
-    console.log('MongoDB Connected');
-    return server.listen({ port: 5000 });
-  })
-  .then((res) => {
+server.listen({ port: 8088 }).then((res) => {
     console.log(`Server running at ${res.url}`);
-  });
+});
